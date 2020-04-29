bc72110cc213e1a18ad43e605d74ad2389e6f206
周源培 <1181588051@qq.com>
2020-04-29 15:51:28 +0800

    调试

--- app/Lib/Common.php
+++ app/Lib/Common.php
@@ -9,14 +9,28 @@ class Common
     public static function getData($url, $decode = true, $assoc = true, $ssl = false){
 
 
-        $header = array('User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36');
+        $useragent = array(
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0)',
+            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.2)',
+            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)',
+            'Mozilla/5.0 (Windows; U; Windows NT 5.2) Gecko/2008070208 Firefox/3.0.1',
+            'Opera/9.27 (Windows NT 5.2; U; zh-cn)',
+            'Opera/8.0 (Macintosh; PPC Mac OS X; U; en)',
+            'Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.27 Safari/525.13 ',
+            'Mozilla/5.0 (Windows; U; Windows NT 5.2) AppleWebKit/525.13 (KHTML, like Gecko) Version/3.1 Safari/525.13'
+        );
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-        curl_setopt($curl,CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0');
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $ssl);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        // 设置iP和useragent
+        curl_setopt($curl, CURLOPT_REFERER, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT, array_rand($useragent));
+
+        //添加这两行
+        curl_setopt($curl, CURLOPT_ENCODING, "gzip"); 
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ["Origin: $url"]);
 
         $rawData = curl_exec($curl);
         if (curl_errno($curl)) {
