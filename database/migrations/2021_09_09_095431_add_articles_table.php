4ec7d5237fb309103d433e6b08b6d8ee394467d5
周源培 <1181588051@qq.com>
2021-10-20 18:15:11 +0800

    fix: 更新表迁移

--- database/migrations/2021_09_09_095431_add_articles_table.php
+++ database/migrations/2021_09_09_095431_add_articles_table.php
@@ -16,9 +16,11 @@ class AddArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title',100)->comment('文章标题');
-            $table->string('cover',150)->default(0)->comemnt('文章封面配图');
+            $table->string('cover',150)->nullable()->comemnt('文章封面配图');
             $table->text('content')->comment('文章具体内容');
             $table->integer('user_id')->default(0)->commnet('文章作者');
+            $table->integer('views')->default(0)->comment('浏览量');
+            $table->integer('praises')->default(0)->comment('点赞量');
             $table->timestamps();
             $table->softDeletes();
         });
